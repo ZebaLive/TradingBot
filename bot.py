f93213160c62b8f4e739f0f11434d748ecfef48c
Paulius <paulius.pluta@arcapay.com>
2023-07-15 16:33:25 +0300

    chart past market data

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 import coinbasepro as cbpro
+import itertools
+import pandas as pd
+import mplfinance as mpf
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -13,6 +16,30 @@ api_passphrase = os.getenv('API_PASSPHRASE')
 client = cbpro.AuthenticatedClient(
     api_key, api_secret, api_passphrase, "https://api-public.sandbox.exchange.coinbase.com")
 
-# Test the connection by fetching your account information
-accounts = client.get_accounts()
-print(accounts)
+# # Test the connection by fetching your account information
+# accounts = client.get_accounts()
+# print(accounts)
+
+product_id = 'BTC-USD'
+
+# Fetch market data
+candles = client.get_product_historic_rates(
+    product_id, granularity=3600)  # Hourly data
+df = pd.DataFrame(candles, columns=[
+                  'time', 'low', 'high', 'open', 'close', 'volume'])
+
+# Convert columns to appropriate data types
+df[['low', 'high', 'open', 'close', 'volume']] = df[[
+    'low', 'high', 'open', 'close', 'volume']].astype(float)
+
+# Convert timestamp to datetime and set as index
+df['time'] = pd.to_datetime(df['time'], unit='s')
+df.set_index('time', inplace=True)
+
+# Calculate a simple moving average (SMA) with a window of 20 periods
+sma_window = 20
+df['SMA'] = df['close'].rolling(sma_window).mean()
+
+# Plot the candlestick chart with SMA overlay
+mpf.plot(df, type='candle', mav=(sma_window,), volume=True,
+         title='BTC-USD Candlestick Chart with SMA')
